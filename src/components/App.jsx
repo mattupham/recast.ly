f13811b1042f16a821ab9da41e2cbf5fcf71a518
mattupham <mcupham@gmail.com>
2017-12-13 21:45:40 -0800

    Implement basic render functions for all components

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,16 +1,17 @@
-var App = () => (
+var App = (props) => (
   <div>
     <nav className="navbar">
       <div className="col-md-6 offset-md-3">
-        <div><h5><em>search</em> view goes here</h5></div>
+        <div><Search /></div>
       </div>
     </nav>
     <div className="row">
       <div className="col-md-7">
-        <div><h5><em>videoPlayer</em> view goes here</h5></div>
+        <div><VideoPlayer video={props.videoCollectionData[0]}/></div>
       </div>
       <div className="col-md-5">
-        <div><h5><em>videoList</em> view goes here</h5></div>
+        {console.log(props.videoCollectionData[0])}
+        <div><VideoList videoCollection={props.videoCollectionData}/></div>
       </div>
     </div>
   </div>
@@ -19,3 +20,11 @@ var App = () => (
 // In the ES6 spec, files are "modules" and do not share a top-level scope
 // `var` declarations will only exist globally where explicitly defined
 window.App = App;
+
+
+
+//exVD = [{obj1}, {obj2}, {obj3}]
+
+// props = {
+//   videoCollection: {"videos"},
+// }
--- src/index.js
+++ src/index.js
@@ -1,2 +1,5 @@
 // TODO: Render the `App` component to the DOM
 
+ReactDOM.render(
+  <App videoCollectionData={exampleVideoData}/>, document.getElementById('app')
+);
